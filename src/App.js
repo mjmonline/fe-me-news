9d3480ffb3c20f889f2c28b2d885941586b6de0f
Mansour J. Mansour <mansour@mjmonline.se>
2018-04-26 13:46:57 +0200

    fixes imports/exports and adds repository to package.json

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React, { Component } from "react";
-import Page from "./components/Page";
-import Header from "./components/Header";
-import Content from "./components/Content";
-import Footer from "./components/Footer";
+import { Page, Header, Content, Footer } from "./components";
 
 class App extends Component {
   render() {
