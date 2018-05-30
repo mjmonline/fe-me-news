fcc13117bd4c7819df72c99b9babbdd4503e9ed1
Mansour J. Mansour <mansour@mjmonline.se>
2018-05-30 13:09:48 +0200

    adds actions and reducers from previous sessions

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,18 +1,7 @@
-// helpers
+const BASE_URL = "https://hacker-news.firebaseio.com";
 const fetchJson = url => fetch(url).then(res => res.json());
-const first10 = arr => arr.slice(0, 10);
-
-const BASEURL = "https://hacker-news.firebaseio.com/v0";
-
-const getItemsIds = () => {
-  return fetchJson(`${BASEURL}/topstories.json`).then(first10);
-};
-
-const getItem = id => {
-  return fetchJson(`${BASEURL}/item/${id}.json`);
-};
 
 export const api = {
-  getItemsIds,
-  getItem
+  getItem: id => fetchJson(`${BASE_URL}/v0/item/${id}.json`),
+  getItemsIds: () => fetchJson(`${BASE_URL}/v0/topstories.json`)
 };
